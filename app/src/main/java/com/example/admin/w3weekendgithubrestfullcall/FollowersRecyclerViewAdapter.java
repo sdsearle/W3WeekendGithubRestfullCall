2e09934d38e8a1c5c96aa96f25d46c0ce3235b06
sdsearle <spencer.seiro@gmail.com>
2017-09-18 13:11:40 -0400

    Added glide for showing image in following or follower recycler views

--- app/src/main/java/com/example/admin/w3weekendgithubrestfullcall/FollowersRecyclerViewAdapter.java
+++ app/src/main/java/com/example/admin/w3weekendgithubrestfullcall/FollowersRecyclerViewAdapter.java
@@ -1,26 +1,16 @@
 package com.example.admin.w3weekendgithubrestfullcall;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.admin.w3weekendgithubrestfullcall.FollowingFollowersModel.Response;
-import com.example.admin.w3weekendgithubrestfullcall.RepoModel.RepoResponse;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
 
 /**
@@ -30,11 +20,12 @@ import java.util.List;
 public class FollowersRecyclerViewAdapter extends RecyclerView.Adapter<FollowersRecyclerViewAdapter.ViewHolder> {
 
     private final List<Response> mValues;
+    Context context;
 
 
     public FollowersRecyclerViewAdapter(Context context, List<Response> items) {
         mValues = items;
-
+        this.context = context;
     }
 
     @Override
@@ -48,53 +39,9 @@ public class FollowersRecyclerViewAdapter extends RecyclerView.Adapter<Followers
     public void onBindViewHolder(final FollowersRecyclerViewAdapter.ViewHolder holder, final int position) {
         holder.mItem = mValues.get(position);
         holder.tvUserName.setText(mValues.get(position).getLogin());
-        holder.ivUserPic.setVisibility(View.INVISIBLE);
-        holder.pbUserPic.setVisibility(View.VISIBLE);
+        holder.ivUserPic.setVisibility(View.VISIBLE);
+        Glide.with(context).load(mValues.get(position).getAvatarUrl()).into(holder.ivUserPic);
 
-        final Handler handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message message) {
-                byte[] byteArray = message.getData().getByteArray("UserPic");
-                Bitmap bm = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
-                holder.ivUserPic.setImageBitmap(bm);
-                holder.ivUserPic.setVisibility(View.VISIBLE);
-                holder.pbUserPic.setVisibility(View.INVISIBLE);
-                return false;
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Bitmap bm = null;
-                try {
-                    InputStream inputStream = new URL(mValues.get(position).getAvatarUrl()).openStream();
-                    bm = BitmapFactory.decodeStream(inputStream);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Bundle bundle = new Bundle();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bm.compress(Bitmap.CompressFormat.PNG,100,baos);
-                bundle.putByteArray("UserPic",baos.toByteArray());
-
-                Message message = new Message();
-                message.setData(bundle);
-                handler.sendMessage(message);
-            }
-        }).start();
-
-
-        /*holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });*/
     }
 
     @Override
@@ -106,7 +53,6 @@ public class FollowersRecyclerViewAdapter extends RecyclerView.Adapter<Followers
         public final View mView;
         public final TextView tvUserName;
         public final ImageView ivUserPic;
-        private final ProgressBar pbUserPic;
         public Response mItem;
 
         public ViewHolder(View view) {
@@ -114,7 +60,6 @@ public class FollowersRecyclerViewAdapter extends RecyclerView.Adapter<Followers
             mView = view;
             tvUserName = (TextView) view.findViewById(R.id.tvUserName);
             ivUserPic = view.findViewById(R.id.ivUserPic);
-            pbUserPic = view.findViewById(R.id.pbUserPic);
         }
 
         @Override
